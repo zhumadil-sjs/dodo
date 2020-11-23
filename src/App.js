5cfbffcb3fd8c04234298d8700332f95a769a4ca
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-11-24 00:11:33 +0600

    header and hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './index.scss';
+import Header from './components/header';
+import Hero from './components/hero';
 
-const App = () => {
-  return <div className="App">
-
-  </div>;
-};
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from '../../assets/svg/logo.svg'
+
+
+const Header =() => {
+    return (
+        <header className='header'>
+            <div className="container">
+                <div className="header__row row">
+                    <div className="header__logo logo">
+                        <img src={logo} alt="logo"/>
+                      <div className="logo__info">
+                          <h2 className="logo__title">REACT PIZZA</h2>
+                          <p className="logo__description">самая вкусная пицца во вселенной</p>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
