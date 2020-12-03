dc113c676e51137d4cb37a4bff0518aaa083f334
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-12-03 09:03:02 +0600

    react js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './index.scss';
-import Header from './components/header';
-import Cart from './pages/cart';
-import { Route } from 'react-router-dom';
-import Home from './pages/home';
-import React,{useEffect} from 'react';
+import "./index.scss";
+import Header from "./components/header";
+import Cart from "./pages/cart";
+import { Route } from "react-router-dom";
+import Home from "./pages/home";
+import React, { useEffect } from "react";
 
 function App() {
   let path;
   useEffect(() => {
     path = window.location.pathname;
-  },[window.location.pathname]);
+  }, [window.location.pathname]);
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      <Route exact path="/" component={ Home } />
-      <Route exact path="/cart" component={ Cart } />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/cart" component={Cart} />
     </div>
   );
 }
