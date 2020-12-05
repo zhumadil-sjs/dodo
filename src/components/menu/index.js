63c93a1a8f7d6591e796d203a59ec94f06a522d9
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-12-05 22:42:27 +0600

    react js and redux

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,28 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import Filter from '../filter';
-import Sort from '../sortPopUp';
-import Pizzas from '../pizzas';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from "react";
+import Filter from "../filter";
+import Sort from "../sortPopUp";
+import Pizzas from "../pizzas";
+import PropTypes from "prop-types";
 
 function Menu() {
   const [pizzas, setPizzas] = useState([]);
   useEffect(async () => {
-    return await fetch('http://localhost:3000/database.json')
+    return await fetch("http://localhost:3000/database.json")
       .then((response) => response.json())
       .then(({ pizzas }) => pizzas)
       .then((array) => setPizzas(array));
   }, []);
 
   return (
-    <section className='menu'>
-      <div className='container'>
-        <div className='row menu__row'>
+    <section className="menu">
+      <div className="container">
+        <div className="row menu__row">
           <Filter />
-          <Sort items={[{name:'Популярности'}, {name:'Цене'}, {name:'Алфавиту'}]} />
+          <Sort
+            items={[
+              { name: "Популярности" },
+              { name: "Цене" },
+              { name: "Алфавиту" },
+            ]}
+          />
         </div>
-        <div className='pizzas'>
-          <h2 className='pizzas__title'>Все пиццы</h2>
-          <div className='row pizzas__row pt-35 ajara'>
+        <div className="pizzas">
+          <h2 className="pizzas__title">Все пиццы</h2>
+          <div className="row pizzas__row pt-35 ajara">
             {pizzas &&
               pizzas.map((item) => {
                 return <Pizzas {...item} key={item.id} />;
@@ -35,5 +41,3 @@ function Menu() {
 }
 
 export default Menu;
-
-
