cc3ef9649f437b4311910a6076c07f707d933816
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-12-05 22:39:18 +0600

    react js and redux

--- src/redux/reducers/sort.js
+++ src/redux/reducers/sort.js
@@ -1,5 +1,5 @@
 const initailValues = {
-  type: 'SET_SORT_BY',
+  type: "SET_SORT_BY",
   payload: 1,
 };
 
@@ -7,9 +7,9 @@ console.log(initailValues);
 
 const setSortBy = (state = initailValues, action) => {
   switch (action.type) {
-    case 'ADD':
+    case "ADD":
       return { ...(state + action.payload) };
-    case 'REMOVE':
+    case "REMOVE":
       return { ...(state - action.payload) };
     default:
       return state;
