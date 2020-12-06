295d4d48a18683738fd0ce17fb6008fa36c885fd
zhumadil-sjs <zhumadil.seiitkulov01@gmail.com>
2020-12-06 22:12:37 +0600

    react js and redux

--- src/components/pizzas/index.js
+++ src/components/pizzas/index.js
@@ -10,6 +10,7 @@ const Pizzas = ({ id, imageUrl, name, price, types, sizes }) => {
   const sizesAvailable = [26, 30, 40];
   const typesNames = ["тонкое", "традиционное"];
 
+  
   const typesClick = (i) => {
     setTypesActive(i);
   };
